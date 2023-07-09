c30325fcf5b5ad6ac366ed95c97382354c939875
Gabriele Bonini <114037656+gabrielebonini99@users.noreply.github.com>
2023-07-09 15:51:01 +0200

    Create cookies.js

---
+++ js/cookies.js
@@ -0,0 +1,2 @@
+let text = document.getElementById("CybotCookiebotDialogBodyContentTitle").innerHTML; 
+document.getElementById("CybotCookiebotDialogBodyContentTitle").innerHTML = text.replace("Questo sito web utilizza i cookie", "This website uses cookies");
