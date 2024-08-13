87798407c02140df9efbbf4688e7c416192f04c5
Gabriele Bonini <114037656+gabrielebonini99@users.noreply.github.com>
2024-08-13 14:45:35 +0200

    Delete js/main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://api.anychat.one/widget/aab9f6f1-654c-4b22-9c02-a7bcef11433d?r=' + encodeURIComponent(window.location);
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'contactus-jssdk'));
