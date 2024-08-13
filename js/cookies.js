f86470cf3fba5a4e5687acaa2559534412aeca7a
Gabriele Bonini <114037656+gabrielebonini99@users.noreply.github.com>
2024-08-13 14:45:47 +0200

    Delete js/cookies.js

--- js/cookies.js
+++
@@ -1,2 +0,0 @@
-let text = document.getElementById("CybotCookiebotDialogBodyContentTitle").innerHTML; 
-document.getElementById("CybotCookiebotDialogBodyContentTitle").innerHTML = text.replace("Questo sito web utilizza i cookie", "This website uses cookies");
